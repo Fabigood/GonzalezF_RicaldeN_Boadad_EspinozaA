55c052526a155afb87967bd9c778295e55a2e1ee
Fabio Gonzalez <fabio.gonzalez.fiallos@udla.edu.ec>
2025-05-05 08:32:58 -0500

    Add project files.

---
+++ GonzalezF_RicaldeN_Boadad_EspinozaA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GonzalezF_RicaldeN_Boadad_EspinozaA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
